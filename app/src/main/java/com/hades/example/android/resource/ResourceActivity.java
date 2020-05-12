a043affce81de6e5ae1416793e835ad2272e23ec
Vicky <ying.vicky.cao@outlook.com>
2020-05-12 16:56:49 +0800

    ImageView add shadow

--- app/src/main/java/com/hades/example/android/resource/ResourceActivity.java
+++ app/src/main/java/com/hades/example/android/resource/ResourceActivity.java
@@ -83,7 +83,7 @@ public class ResourceActivity extends PermissionActivity {
 
     @Override
     protected void showCurrentTest() {
-        pageThemeChoose();
+        pageLevelListDrawable();
     }
 
     private void pageColor() {
