146cde6be73bf85c9541d17e0eb0688ddfe678d1
YingVickyCao <ying.vicky.cao@outlook.com>
2020-06-05 15:12:38 +0800

    test WebView

--- app/src/main/java/com/hades/example/android/widget/WidgetActivity.java
+++ app/src/main/java/com/hades/example/android/widget/WidgetActivity.java
@@ -106,7 +106,7 @@ public class WidgetActivity extends NoNeedPermissionActivity implements IItemCli
 
     @Override
     protected void showCurrentTest() {
-        pageListView();
+        pageWebView();
     }
 
     private void pageViewLocation() {
--- app/src/main/java/com/hades/example/android/widget/webview/TestWebViewFragment.java
+++ app/src/main/java/com/hades/example/android/widget/webview/TestWebViewFragment.java
@@ -1,6 +1,7 @@
 package com.hades.example.android.widget.webview;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +26,21 @@ public class TestWebViewFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mWebView.loadUrl("file:///android_asset/web/maven.html");
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        // asserts folder
+//        mWebView.loadUrl("file:///android_asset/web/maven.html");
+
+        // asserts folder
+//        mWebView.loadUrl("file:///android_asset/web/full/index.html"); // not ok. Can Swipe pages up/down,but content is white
+//        mWebView.loadUrl("file:///android_asset/web/full/index.html?page=1");// not ok. Can Swipe pages up/down,but content is white
+//        mWebView.loadUrl("file:///android_asset/web/full/1.html");// not ok, page 1 is showed,but sth is lost, e.g.,bg or grid
+
+        // /sdcard/maven.html
+//        mWebView.loadUrl("file:///" + Environment.getExternalStorageDirectory().getPath() + "/maven.html");// ok
+//        mWebView.loadUrl("file:///sdcard/maven.html"); // ok
+//        mWebView.loadUrl("file:///sdcard/full/index.html"); // not ok. Can Swipe pages up/down,but content is white
+//        mWebView.loadUrl("file:///sdcard/full/index.html?page=1"); // not ok. Can Swipe pages up/down,but content is white
+        mWebView.loadUrl("file:///sdcard/full/1.html"); // not ok, page 1 is showed,but sth is lost, e.g.,bg or grid
     }
 }
