6d3d8415cff2390fc419516c4272383ab87865f3
YingVickyCao <ying.vicky.cao@outlook.com>
2022-02-11 15:26:10 +0800

    toast

--- app/src/main/java/com/hades/example/android/other_ui/_toast/TestToastFragment.java
+++ app/src/main/java/com/hades/example/android/other_ui/_toast/TestToastFragment.java
@@ -3,10 +3,12 @@ package com.hades.example.android.other_ui._toast;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -36,6 +38,8 @@ public class TestToastFragment extends Fragment {
         view.findViewById(R.id.toast_with_icon_in_light_theme).setOnClickListener(v -> toastWithIconInLightTheme());
         view.findViewById(R.id.toast_with_icon_in_light_theme2).setOnClickListener(v -> toastWithIconInLightTheme2());
         view.findViewById(R.id.toast_with_icon_in_light_theme3).setOnClickListener(v -> toastWithIconInLightTheme3());
+        view.findViewById(R.id.toast_4_custom_width_and_position).setOnClickListener(v -> toast_4_custom_width_and_position_v2());
+        view.findViewById(R.id.toast_with_icon_4_custom_width_and_position).setOnClickListener(v -> toast_with_icon_4_custom_width_and_position());
         root = view.findViewById(R.id.root);
         return view;
     }
@@ -140,6 +144,46 @@ public class TestToastFragment extends Fragment {
         toast.setGravity(Gravity.CENTER, 0, 0);
 
         toast.setView(LayoutInflater.from(getUsedContext()).inflate(R.layout.toast_custom_with_icon_light3, null));
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    // Recommend。toast为具体底部100dp，距离左右屏幕为8dp
+    private void toast_4_custom_width_and_position() {
+        Toast toast = new Toast(getUsedContext());
+        FrameLayout toastLayout = (FrameLayout) LayoutInflater.from(getUsedContext()).inflate(R.layout.toast_custom_4_custom_width_and_position, null);
+        toast.setGravity(Gravity.BOTTOM | Gravity.FILL_HORIZONTAL, 0, 0);
+        toast.setView(toastLayout);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    // Depressed。toast为具体底部100dp，距离左右屏幕为8dp
+    private void toast_4_custom_width_and_position_v2() {
+        Toast toast = new Toast(getUsedContext());
+        FrameLayout toastLayout = (FrameLayout) LayoutInflater.from(getUsedContext()).inflate(R.layout.toast_custom_4_custom_width_and_position, null);
+
+        FrameLayout messageRoot = toastLayout.findViewById(R.id.messageRoot);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int widthPixels = displayMetrics.widthPixels - getResources().getDimensionPixelSize(R.dimen.size_16);
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(widthPixels, FrameLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.leftMargin = getResources().getDimensionPixelSize(R.dimen.size_8);
+        layoutParams.rightMargin = getResources().getDimensionPixelSize(R.dimen.size_8);
+        messageRoot.setLayoutParams(layoutParams);
+
+        toast.setGravity(Gravity.BOTTOM, 0, getResources().getDimensionPixelSize(R.dimen.size_100));
+        toast.setView(toastLayout);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    private void toast_with_icon_4_custom_width_and_position() {
+        Toast toast = new Toast(getUsedContext());
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+
+        toast.setView(LayoutInflater.from(getUsedContext()).inflate(R.layout.toast_custom_with_icon_4_custom_width_and_position, null));
         toast.setDuration(Toast.LENGTH_SHORT);
         toast.show();
     }
