c8f392dfa3b52252882b443d20ddc5425ba00ff8
YingVickyCao <ying.vicky.cao@outlook.com>
2022-03-09 21:04:59 +0800

    dimension test

--- app/src/main/java/com/hades/example/android/resource/ResourceActivity.java
+++ app/src/main/java/com/hades/example/android/resource/ResourceActivity.java
@@ -84,7 +84,7 @@ public class ResourceActivity extends PermissionActivity {
 
     @Override
     protected void showCurrentTest() {
-        pageFont();
+        pageDimension();
     }
 
     private void pageColor() {
--- app/src/main/java/com/hades/example/android/resource/dimension/TestDimensionFragment.java
+++ app/src/main/java/com/hades/example/android/resource/dimension/TestDimensionFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.DimenRes;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -26,6 +27,7 @@ public class TestDimensionFragment extends Fragment {
         View view = inflater.inflate(R.layout.res_dimension, container, false);
         view.findViewById(R.id.px2dp).setOnClickListener(v -> px2dp());
         view.findViewById(R.id.dp2px).setOnClickListener(v -> dp2px());
+        view.findViewById(R.id.test_dimen).setOnClickListener(v -> test_dimen());
         return view;
     }
 
@@ -47,5 +49,30 @@ public class TestDimensionFragment extends Fragment {
         float px3 = (100f * scale); // 262.5
         Log.d(TAG, "dp2px: px3 = " + px3);
 
+    }
+
+    private void test_dimen() {
+        /**
+         * 1 getDimension 返回值是float。
+         * getDimensionPixelSize 返回值是int，四舍五入
+         * getDimensionPixelOffset 返回值是int，只保留整数部分。
+         *
+         * 2 getDimension，getDimensionPixelSize，getDimensionPixelOffset 返回为实际值。
+         * 若为dp/sp，则返回xml中定义的value * 屏幕密度。
+         * 若为px，则返回xml中定义的value。
+         */
+        // dp->3.5,4.0,3.0
+        test_dimen("dp", R.dimen.dp_01);
+        // sp->3.5,4.0,3.0
+        test_dimen("sp", R.dimen.sp_01);
+        // px->1.0,1.0,1.0
+        test_dimen("px", R.dimen.px_01);
+    }
+
+    private void test_dimen(String print, @DimenRes int id) {
+        float valueOfDimension = getResources().getDimension(id);
+        float valueOfDimensionPixelSize = getResources().getDimensionPixelSize(id);
+        float valueOfDimensionPixelOffset = getResources().getDimensionPixelOffset(id);
+        Log.d(TAG, "test_dimen: " + print + "->" + valueOfDimension + "," + valueOfDimensionPixelSize + "," + valueOfDimensionPixelOffset);
     }
 }
