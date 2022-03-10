b42867a10173b81111c26d4f6fc07f0725f8f45b
YingVickyCao <ying.vicky.cao@outlook.com>
2022-03-10 14:23:47 +0800

    LinearLayout border

--- app/src/main/java/com/hades/example/android/widget/WidgetActivity.java
+++ app/src/main/java/com/hades/example/android/widget/WidgetActivity.java
@@ -112,7 +112,7 @@ public class WidgetActivity extends NoNeedPermissionActivity implements IItemCli
 
     @Override
     protected void showCurrentTest() {
-        pageConstraintLayout();
+        pageLinearLayout();
     }
 
     private void pageViewLocation() {
