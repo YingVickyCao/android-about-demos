45f11e85c47eec8d5ef2a2e0180fa015f287952b
YingVickyCao <ying.vicky.cao@outlook.com>
2022-04-20 11:55:02 +0800

    image button

--- app/src/main/java/com/hades/example/android/widget/TestImageButtonFragment.java
+++ app/src/main/java/com/hades/example/android/widget/TestImageButtonFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,14 +13,34 @@ import androidx.fragment.app.Fragment;
 import com.hades.example.android.R;
 
 public class TestImageButtonFragment extends Fragment {
+    ImageButton btn1;
+    ImageButton btn2;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.widget_image_button_page, container, false);
-        view.findViewById(R.id.testEnable).setEnabled(true);
-        view.findViewById(R.id.testEnable).setClickable(true);
-        view.findViewById(R.id.testEnable2).setEnabled(true);
-        view.findViewById(R.id.testEnable2).setClickable(true);
+        btn1 = view.findViewById(R.id.btn1);
+        view.findViewById(R.id.enable_true).setOnClickListener(v -> {
+            btn1.setEnabled(true);
+            btn1.setClickable(true);
+        });
+
+        view.findViewById(R.id.enable_false).setOnClickListener(v -> {
+            btn1.setEnabled(false);
+            btn1.setClickable(false);
+        });
+
+        btn2 = view.findViewById(R.id.btn2);
+        view.findViewById(R.id.selected_true).setOnClickListener(v -> {
+            btn2.setSelected(true);
+            btn2.setClickable(true);
+        });
+
+        view.findViewById(R.id.selected_false).setOnClickListener(v -> {
+            btn2.setSelected(false);
+            btn2.setClickable(false);
+        });
         return view;
     }
 }
