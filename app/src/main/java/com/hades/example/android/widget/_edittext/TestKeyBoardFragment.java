38ca225d7b5f162e3712b280091f33a40d1ef6fa
YingVickyCao <ying.vicky.cao@outlook.com>
2022-04-20 20:40:01 +0800

    防止快速点击

--- app/src/main/java/com/hades/example/android/widget/_edittext/TestKeyBoardFragment.java
+++ app/src/main/java/com/hades/example/android/widget/_edittext/TestKeyBoardFragment.java
@@ -4,7 +4,7 @@ import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -19,6 +19,7 @@ import androidx.fragment.app.Fragment;
 import com.hades.example.android.R;
 
 public class TestKeyBoardFragment extends Fragment {
+    private static final String TAG = "TestKeyBoardFragment";
 
     private EditText editText;
     private KeyboardView keyboardView;
@@ -38,7 +39,13 @@ public class TestKeyBoardFragment extends Fragment {
         editText.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                // Fix：反复点击EditText，点击的太快，键盘会闪。
+                // 解决：增加200ms的重复事件的过滤
+                if (ButtonUtils.isFastClick()) {
+                    return false;
+                }
                 if (!editText.hasFocus()) {
+                    Log.d(TAG, "onTouch: !editText.hasFocus():go here");
                     // setInputType 调用或不调用，显现一样，因此注释掉该代码
 //                    int inputType = editText.getInputType();
 //                    editText.setInputType(InputType.TYPE_NULL);
@@ -46,11 +53,12 @@ public class TestKeyBoardFragment extends Fragment {
 //                    editText.setInputType(inputType);
                 } else {
                     if (keyboardView.getVisibility() != View.VISIBLE) { // Fix：显示和不可见自定义键盘多次后，再次点击EditText，自定义键盘不显示。
+                        Log.d(TAG, "onTouch: else keyboard is not visible, show it");
                         showKeyboard();
                     } else {
+                        Log.d(TAG, "onTouch: else keyboard is visible, hide it");
                         hideKeyboard();
                     }
-
                 }
                 return false;
             }
@@ -63,6 +71,8 @@ public class TestKeyBoardFragment extends Fragment {
     private void forbidSystemKeyBoard() {
         // 禁用软键盘
         getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
         // 在需要打开的Activity取消禁用软键盘
         //getWindow().clearFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
     }
@@ -83,7 +93,7 @@ public class TestKeyBoardFragment extends Fragment {
         keyboard = new Keyboard(editText.getContext(), R.xml.keyboard_numbers);
         keyboardView.setKeyboard(keyboard);
         keyboardView.setEnabled(true);
-        keyboardView.setPreviewEnabled(false);
+        keyboardView.setPreviewEnabled(false); // 开启和关闭点击Key的预览效果
         keyboardView.setOnKeyboardActionListener(new KeyboardView.OnKeyboardActionListener() {
             @Override
             public void swipeUp() {
@@ -126,9 +136,7 @@ public class TestKeyBoardFragment extends Fragment {
                         }
                         break;
                     case Keyboard.KEYCODE_CANCEL:
-                        keyboardView.setVisibility(View.GONE);
-                        // Fix: 显示和不可见自定义键盘多次后，再次点击EditText，自定义键盘不显示。
-                        editText.clearFocus();
+                        hideKeyboard();
                         break;
 
                     default:
