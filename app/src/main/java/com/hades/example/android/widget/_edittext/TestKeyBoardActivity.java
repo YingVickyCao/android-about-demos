e0f1e6f9380f532e79cefd3234a662aaa4ea64ea
YingVickyCao <ying.vicky.cao@outlook.com>
2022-04-20 16:09:27 +0800

    KeyBoard

--- app/src/main/java/com/hades/example/android/widget/_edittext/TestKeyBoardActivity.java
+++ app/src/main/java/com/hades/example/android/widget/_edittext/TestKeyBoardActivity.java
@@ -1,8 +1,10 @@
 package com.hades.example.android.widget._edittext;
 
 import android.app.Activity;
+import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputType;
 import android.view.MotionEvent;
 import android.view.View;
@@ -14,6 +16,7 @@ public class TestKeyBoardActivity extends Activity {
 
     private EditText editText;
     private KeyboardView keyboardView;
+    private Keyboard keyboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +28,11 @@ public class TestKeyBoardActivity extends Activity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (!editText.hasFocus()) {
-                    int inputback = editText.getInputType();
+                    int inputType = editText.getInputType();
                     editText.setInputType(InputType.TYPE_NULL);
                     showKeyboard();
-                    new KeyboardUtil(keyboardView, editText).showKeyboard();
-                    editText.setInputType(inputback);
+                    init();
+                    editText.setInputType(inputType);
                 } else {
                     hideKeyboard();
                 }
@@ -38,6 +41,66 @@ public class TestKeyBoardActivity extends Activity {
         });
     }
 
+    private void init() {
+        keyboardView.setOnKeyboardActionListener(new KeyboardView.OnKeyboardActionListener() {
+            @Override
+            public void swipeUp() {
+            }
+
+            @Override
+            public void swipeRight() {
+            }
+
+            @Override
+            public void swipeLeft() {
+            }
+
+            @Override
+            public void swipeDown() {
+            }
+
+            @Override
+            public void onText(CharSequence text) {
+            }
+
+            @Override
+            public void onRelease(int primaryCode) {
+            }
+
+            @Override
+            public void onPress(int primaryCode) {
+            }
+
+            @Override
+            public void onKey(int primaryCode, int[] keyCodes) {
+                Editable editable = editText.getText();
+                int start = editText.getSelectionStart();
+                switch (primaryCode) {
+                    case Keyboard.KEYCODE_DELETE:
+                        if (editable != null && editable.length() > 0) {
+                            if (start > 0) {
+                                editable.delete(start - 1, start);
+                            }
+                        }
+                        break;
+                    case Keyboard.KEYCODE_CANCEL:
+                        keyboardView.setVisibility(View.GONE);
+                        editText.clearFocus();
+                        break;
+
+                    default:
+                        editable.insert(start, Character.toString((char) primaryCode));
+                        break;
+                }
+            }
+        });
+        keyboard = new Keyboard(editText.getContext(), R.xml.qwerty);
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setEnabled(true);
+        keyboardView.setPreviewEnabled(false);
+    }
+
+    // Activity中获取焦点时调用，显示出键盘
     public void showKeyboard() {
         int visibility = keyboardView.getVisibility();
         if (visibility == View.GONE || visibility == View.INVISIBLE) {
