bdfaf6d750bcf3da0b5c586613286875ef225739
YingVickyCao <ying.vicky.cao@outlook.com>
2022-04-22 16:34:31 +0800

    Update MyView2.java

--- app/src/main/java/com/hades/example/android/widget/custom_view/MyView2.java
+++ app/src/main/java/com/hades/example/android/widget/custom_view/MyView2.java
@@ -3,9 +3,11 @@ package com.hades.example.android.widget.custom_view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -13,14 +15,14 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 import com.hades.example.android.R;
-import com.hades.example.android.resource.ResourceActivity;
 
 public class MyView2 extends View {
     private static final String TAG = "MyView2";
 
     private RectF rectF = new RectF();
+    private Paint paint = new Paint();
     private Path trianglePath = new Path();
-    private Path pentagonPath = new Path();
+    Shader shader = new LinearGradient(0, 0, 40, 60, new int[]{Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW}, null, Shader.TileMode.REPEAT);
 
     public MyView2(Context context) {
         super(context);
@@ -52,7 +54,6 @@ public class MyView2 extends View {
          */
         canvas.drawColor(getResources().getColor(R.color.paint_bg, getContext().getTheme()));
 
-        Paint paint = new Paint();
         paint.setAntiAlias(true);
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.STROKE);
@@ -102,6 +103,34 @@ public class MyView2 extends View {
         trianglePath.moveTo(sizeNum_10, (float) viewHeight * 7 / 10); // .1
         trianglePath.lineTo((float) viewWidth / 5 + sizeNum_10, (float) viewHeight * 7 / 10);   //.1 -----> .2
         trianglePath.lineTo((float) viewWidth / 10 + sizeNum_10, (float) viewHeight * 6 / 10); // 2. ----->.3
+        trianglePath.close();   // .3 -> .1
+        canvas.drawPath(trianglePath, paint);
+        /**
+         *Second Column
+         */
+        paint.setColor(Color.GREEN);
+        paint.setStyle(Paint.Style.FILL);
+
+        canvas.drawCircle((float) viewWidth / 3 + sizeNum_10, (float) viewWidth / 10 + sizeNum_10, (float) viewWidth / 10, paint);
+        canvas.drawRect((float) viewWidth / 5 + sizeNum_20, (float) viewWidth / 5 + sizeNum_20, (float) viewWidth * 2 / 5 + sizeNum_20, (float) viewWidth * 2 / 5 + sizeNum_20, paint);
+        canvas.drawRect((float) viewWidth / 5 + sizeNum_20, (float) viewHeight / 4 + sizeNum_10 * 3, (float) viewWidth * 2 / 5 + sizeNum_20, (float) viewHeight / 4 + sizeNum_10 * 7, paint);
+
+        rectF.left = (float) viewWidth / 5 + sizeNum_20;
+        rectF.top = (float) (viewHeight / 3) + sizeNum_10 * 3;
+        rectF.right = (float) viewWidth * 2 / 5 + sizeNum_20;
+        rectF.bottom = (float) (viewHeight / 3) + sizeNum_20 * 4;
+        canvas.drawRoundRect(rectF, borderSize, borderSize, paint);
+
+        rectF.left = (float) viewWidth / 5 + sizeNum_20;
+        rectF.top = (float) (viewHeight / 2);
+        rectF.right = (float) viewWidth * 2 / 5 + sizeNum_20;
+        rectF.bottom = (float) (viewHeight / 2) + sizeNum_10 * 3;
+        canvas.drawOval(rectF, paint);
+
+        trianglePath.reset();
+        trianglePath.moveTo((float) viewWidth / 5 + sizeNum_20, (float) viewHeight * 7 / 10); // .1
+        trianglePath.lineTo((float) viewWidth * 2 / 5 + sizeNum_20, (float) viewHeight * 7 / 10);   //.1 -----> .2
+        trianglePath.lineTo((float) viewWidth * 3 / 10 + sizeNum_20, (float) viewHeight * 6 / 10); // 2. ----->.3
         trianglePath.close();   // .3 -> .1
         canvas.drawPath(trianglePath, paint);
     }
