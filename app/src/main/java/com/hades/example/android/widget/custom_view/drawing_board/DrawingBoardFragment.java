1a872202731f7ed107778c5611a937655a9d1c92
YingVickyCao <ying.vicky.cao@outlook.com>
2022-04-27 14:40:24 +0800

    Custom View - DrawingBoardView

--- app/src/main/java/com/hades/example/android/widget/custom_view/drawing_board/DrawingBoardFragment.java
+++ app/src/main/java/com/hades/example/android/widget/custom_view/drawing_board/DrawingBoardFragment.java
@@ -1,5 +1,6 @@
 package com.hades.example.android.widget.custom_view.drawing_board;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,8 @@ import com.hades.example.android.R;
  * 利用双缓冲实现画面板
  */
 public class DrawingBoardFragment extends Fragment {
+    DrawingBoardView drawingBoardView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,37 +30,40 @@ public class DrawingBoardFragment extends Fragment {
         view.findViewById(R.id.width5).setOnClickListener(v -> width5());
         view.findViewById(R.id.blur).setOnClickListener(v -> width5());
         view.findViewById(R.id.emboss).setOnClickListener(v -> emboss());
+
+//        drawingBoardView = view.findViewById(R.id.drawingBoardView);
         return view;
     }
 
     private void colorRed() {
-
+        drawingBoardView.setPaintColor(Color.RED);
     }
 
     private void colorGreen() {
+        drawingBoardView.setPaintColor(Color.GREEN);
     }
 
     private void colorBlue() {
-
+        drawingBoardView.setPaintColor(Color.BLUE);
     }
 
     private void width1() {
-
+        drawingBoardView.setPaintStrokeWidth(getResources().getDimension(R.dimen.size_1));
     }
 
     private void width3() {
-
+        drawingBoardView.setPaintStrokeWidth(getResources().getDimension(R.dimen.size_3));
     }
 
     private void width5() {
-
+        drawingBoardView.setPaintStrokeWidth(getResources().getDimension(R.dimen.size_5));
     }
 
     private void blur() {
-
+        drawingBoardView.useBlurMaskFilter();
     }
 
     private void emboss() {
-
+        drawingBoardView.useEmbossMaskFilter();
     }
 }
--- app/src/main/java/com/hades/example/android/widget/custom_view/drawing_board/DrawingBoardView.java
+++ app/src/main/java/com/hades/example/android/widget/custom_view/drawing_board/DrawingBoardView.java
@@ -2,28 +2,39 @@ package com.hades.example.android.widget.custom_view.drawing_board;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.EmbossMaskFilter;
+import android.graphics.MaskFilter;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import com.hades.example.android.R;
+
 public class DrawingBoardView extends View {
+    private static final String TAG = "DrawingBoardView";
     /**
      * 定义记录前一个拖动事件此点的坐标
      */
     float prevX;
-    float preY;
+    float prevY;
 
     private Path path = new Path();
     private Paint paint = new Paint();
     private Paint bmpPaint = new Paint();
     private Bitmap cachedBitmap = null;
-    Canvas cachedCanvas = null;
+    private Canvas cachedCanvas = null;
+
+    private final EmbossMaskFilter embossMaskFilter = new EmbossMaskFilter(new float[]{1.5f, 1.5f, 1.5f}, 0.6f, 6, 4.2f);
+    private final BlurMaskFilter blurMaskFilter = new BlurMaskFilter(8, BlurMaskFilter.Blur.NORMAL);
+    private MaskFilter maskFilter;
 
     public DrawingBoardView(Context context) {
         super(context);
@@ -31,8 +42,9 @@ public class DrawingBoardView extends View {
 
     public DrawingBoardView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        Log.d(TAG, "DrawingBoardView: ");
 
-        cachedBitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+        cachedBitmap = Bitmap.createBitmap(500, 200, Bitmap.Config.ARGB_8888);
 
         cachedCanvas = new Canvas();
         cachedCanvas.setBitmap(cachedBitmap);
@@ -40,17 +52,35 @@ public class DrawingBoardView extends View {
         paint = new Paint(Paint.DITHER_FLAG);
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.STROKE);
+        setPaintStrokeWidth(getResources().getDimension(R.dimen.size_1));
         paint.setAntiAlias(true);
         paint.setDither(true);
-
     }
 
     public DrawingBoardView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        Log.d(TAG, "DrawingBoardView: ");
     }
 
     public DrawingBoardView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        Log.d(TAG, "DrawingBoardView: ");
+    }
+
+    public void setPaintStrokeWidth(float paintStrokeWidth) {
+        paint.setStrokeWidth(paintStrokeWidth);
+    }
+
+    public void setPaintColor(int paintColor) {
+        paint.setColor(paintColor);
+    }
+
+    public void useEmbossMaskFilter() {
+        paint.setMaskFilter(embossMaskFilter);
+    }
+
+    public void useBlurMaskFilter() {
+        paint.setMaskFilter(blurMaskFilter);
     }
 
     @Override
@@ -60,21 +90,23 @@ public class DrawingBoardView extends View {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "onTouchEvent: ACTION_DOWN");
                 path.moveTo(x, y);
                 prevX = x;
-                preY = y;
+                prevY = y;
                 break;
             case MotionEvent.ACTION_MOVE:
-                path.quadTo(prevX, preY, x, y);
+                Log.d(TAG, "onTouchEvent: ACTION_MOVE");
+                path.quadTo(prevX, prevY, x, y);
                 prevX = x;
-                preY = y;
+                prevY = y;
                 break;
 
             case MotionEvent.ACTION_UP:
+                Log.d(TAG, "onTouchEvent: ACTION_UP");
                 cachedCanvas.drawPath(path, paint);
-                path.reset();
+//                path.reset();
                 break;
-
         }
 
         invalidate();
@@ -83,7 +115,10 @@ public class DrawingBoardView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
+        Log.d(TAG, "onDraw: ");
         bmpPaint.reset();
+
+        canvas.drawColor(getResources().getColor(R.color.paint_bg, getContext().getTheme()));
         canvas.drawBitmap(cachedBitmap, 0, 0, bmpPaint);
         canvas.drawPath(path, paint);
     }
