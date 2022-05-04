3b8dc886b928712e968ecd58337726042420d586
YingVickyCao <ying.vicky.cao@outlook.com>
2022-05-04 16:31:48 +0800

    Z Axis

---
+++ app/src/main/java/com/hades/example/android/widget/ZAxisFragment.java
@@ -0,0 +1,22 @@
+package com.hades.example.android.widget;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.hades.example.android.R;
+
+public class ZAxisFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.widget_z_axis, container, false);
+        return view;
+    }
+}
