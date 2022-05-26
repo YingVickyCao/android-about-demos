97bf6af9db2b7b459a2197bb1dbc36a6989c273e
YingVickyCao <ying.vicky.cao@outlook.com>
2022-05-26 20:54:13 +0800

    Xfermode

---
+++ app/src/main/java/com/hades/example/android/widget/custom_view/Xfermode/PorterDuffModeExampleView_Add.java
@@ -0,0 +1,47 @@
+package com.hades.example.android.widget.custom_view.Xfermode;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Xfermode;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.hades.example.android.R;
+
+public class PorterDuffModeExampleView_Add extends View {
+    private static final String TAG = PorterDuffModeExampleView_Add.class.getSimpleName();
+
+    private final Paint paint = new Paint();
+
+    // 和 Google 一致
+//    private Bitmap destinationImage = BitmapFactory.decodeResource(getResources(), R.drawable.ic_composite_dst);
+//    private Bitmap sourceImage = BitmapFactory.decodeResource(getResources(), R.drawable.ic_composite_src);
+
+    private Bitmap destinationImage = BitmapFactory.decodeResource(getResources(), R.drawable.ic_composite_dst_2);
+    private Bitmap sourceImage = BitmapFactory.decodeResource(getResources(), R.drawable.ic_composite_src_2);
+
+    private Xfermode xfermode = new PorterDuffXfermode(PorterDuff.Mode.ADD);
+
+    public PorterDuffModeExampleView_Add(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int saveCount = canvas.saveLayer(0, 0, getWidth(), getHeight(), paint);
+
+        canvas.drawBitmap(destinationImage, 0, 0, paint);
+        paint.setXfermode(xfermode);
+        canvas.drawBitmap(sourceImage, 0, 0, paint);
+        paint.setXfermode(null);
+
+        canvas.restoreToCount(saveCount);
+    }
+}
