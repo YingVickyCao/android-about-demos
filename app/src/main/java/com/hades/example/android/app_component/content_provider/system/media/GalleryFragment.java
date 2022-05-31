da2cc71e286c8ee8227594aa0a12b4165c4589f9
YingVickyCao <ying.vicky.cao@outlook.com>
2022-05-31 18:43:09 +0800

    Gallery

--- app/src/main/java/com/hades/example/android/app_component/content_provider/system/media/GalleryFragment.java
+++ app/src/main/java/com/hades/example/android/app_component/content_provider/system/media/GalleryFragment.java
@@ -1,6 +1,5 @@
 package com.hades.example.android.app_component.content_provider.system.media;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
