0b94893526105f1f1e0d3a5f8ed114f620747c9d
YingVickyCao <ying.vicky.cao@outlook.com>
2022-06-09 20:01:41 +0800

    Update ReflectionImageView.java

--- app/src/main/java/com/hades/example/android/widget/custom_view/shader/ReflectionImageView.java
+++ app/src/main/java/com/hades/example/android/widget/custom_view/shader/ReflectionImageView.java
@@ -59,16 +59,17 @@ public class ReflectionImageView extends View {
         matrix.setScale(1F, -1F);
         Bitmap reflection = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
 
-        // 新建图层，它的区域包含了到影图
+        // 新建图层，它的区域包含了倒置图
         int savedCount = canvas.saveLayer(x, y + source.getHeight(), x + source.getWidth(), y + source.getHeight() * 2, null);
-        // 绘制倒影图
+        // ·
         canvas.drawBitmap(reflection, x, y + source.getHeight(), null);
 
         // 设置混合模式
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
         // Shader 用于填充混合目标，颜色从Color.BLACK到透明。
+        // 倒影的高度是源图的1/2
         paint.setShader(new LinearGradient(x, y + source.getHeight(), x, y + source.getHeight() * 1.5f, Color.BLACK, Color.TRANSPARENT, Shader.TileMode.CLAMP));
-        // 使用矩形，作为混合目标，用来裁剪到影图
+        // 使用矩形，作为混合目标，用来裁剪到倒影
         canvas.drawRect(x, y + source.getHeight(), x + reflection.getWidth(), y + source.getHeight() * 2, paint);
         paint.setXfermode(null);
 
