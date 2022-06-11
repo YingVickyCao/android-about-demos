f091c92edc78600be53506eb70129bd733ec69bc
YingVickyCao <ying.vicky.cao@outlook.com>
2022-06-11 11:14:57 +0800

    refactor

--- app/src/main/java/com/hades/example/android/resource/drawable/TestDrawableActivity.java
+++ app/src/main/java/com/hades/example/android/resource/drawable/TestDrawableActivity.java
@@ -1,14 +1,12 @@
 package com.hades.example.android.resource.drawable;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import com.hades.example.android.R;
 import com.hades.example.android.base.BaseActivity;
 import com.hades.example.android.resource.drawable._bitmap.BitmapActivity;
-import com.hades.example.android.resource.drawable._bitmap.TestBitmapMemoryAndScreenDensityFragment;
 import com.hades.example.android.resource.drawable._level_list.TestLevelListDrawableFragment;
 import com.hades.example.android.resource.drawable.clip.TestClipDrawableFragment;
 import com.hades.example.android.resource.drawable.layer.TestLayerDrawableFragment;
@@ -23,10 +21,10 @@ public class TestDrawableActivity extends BaseActivity {
         setContentView(R.layout.res_drawable_activity_layout);
         initViews(R.id.root);
         findViewById(R.id.pageDrawableFolder).setOnClickListener(v -> pageDrawableFolder());
-        findViewById(R.id.pageShapeDrawable).setOnClickListener(v -> pageShapeDrawable());
         findViewById(R.id.pageStateDrawable).setOnClickListener(v -> pageStateDrawable());
         findViewById(R.id.pageVectorDrawable).setOnClickListener(v -> pageVectorDrawable());
         findViewById(R.id.pageBitmap).setOnClickListener(v -> pageBitmap());
+        findViewById(R.id.pageShapeDrawable).setOnClickListener(v -> pageShapeDrawable());
         findViewById(R.id.pageLevelListDrawable).setOnClickListener(v -> pageLevelListDrawable());
         findViewById(R.id.pageLayerListDrawable).setOnClickListener(v -> pageLayerListDrawable());
         findViewById(R.id.pageClipDrawable).setOnClickListener(v -> pageClipDrawable());
@@ -43,14 +41,13 @@ public class TestDrawableActivity extends BaseActivity {
     private void pageVectorDrawable() {
         showFragment(new TestVectorDrawableFragment());
     }
+    
+    private void pageBitmap() {
+        showActivity(BitmapActivity.class);
+    }
 
     private void pageShapeDrawable() {
         showFragment(new TestShapeDrawableFragment());
-    }
-
-
-    private void pageBitmap() {
-        showActivity(BitmapActivity.class);
     }
 
     private void pageLevelListDrawable() {
--- app/src/main/java/com/hades/example/android/resource/drawable/shape/TestShapeDrawableFragment.java
+++ app/src/main/java/com/hades/example/android/resource/drawable/shape/TestShapeDrawableFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 
 import com.hades.example.android.R;
 import com.hades.example.android.base.BaseFragment;
@@ -29,7 +30,7 @@ public class TestShapeDrawableFragment extends BaseFragment {
          * 20,20,20
          * No matter Shape Drawable put in any drawable folder, 不进行放缩
          */
-        Drawable drawable = getResources().getDrawable(R.drawable.drawable_shape_4_divider_vertical);
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.drawable_shape_4_divider_vertical, getContext().getTheme());
         Log.d(TAG, "checkIntrinsicHeight:drawable intrinsicHeight=" + drawable.getIntrinsicHeight());
         Log.d(TAG, "checkIntrinsicHeight:drawable minimumHeight=" + drawable.getMinimumHeight());
         Log.d(TAG, "checkIntrinsicHeight:R.dimen.size_5= " + getResources().getDimension(R.dimen.size_5));
