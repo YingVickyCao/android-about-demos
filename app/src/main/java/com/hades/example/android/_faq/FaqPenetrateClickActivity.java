6b0364127f285c6636855bbe4a58ad47dd003ac2
YingVickyCao <ying.vicky.cao@outlook.com>
2022-08-04 10:12:25 +0800

    player 控制层

--- app/src/main/java/com/hades/example/android/_faq/FaqPenetrateClickActivity.java
+++ app/src/main/java/com/hades/example/android/_faq/FaqPenetrateClickActivity.java
@@ -1,4 +1,4 @@
-package com.hades.example.android._faq.penetrateClick;
+package com.hades.example.android._faq;
 
 import android.os.Bundle;
 import android.util.Log;
---
+++ app/src/main/java/com/hades/example/android/_faq/FaqPlayerControllerPlayerActivity.java
@@ -0,0 +1,21 @@
+package com.hades.example.android._faq;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hades.example.android.R;
+
+/**
+ * Video播放控制层
+ */
+public class FaqPlayerControllerPlayerActivity extends AppCompatActivity {
+    private static final String TAG = FaqPlayerControllerPlayerActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.faq_player_controller_layer);
+    }
+}
