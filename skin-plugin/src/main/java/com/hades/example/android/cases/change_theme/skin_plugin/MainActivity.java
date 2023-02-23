627f0e7e707bdc7e5ac8e258a69c0bfd8f9dbccc
Vicky <ying.vicky.cao@outlook.com>
2023-02-23 22:11:57 +0800

    add skins

---
+++ skin-plugin/src/main/java/com/hades/example/android/cases/change_theme/skin_plugin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hades.example.android.cases.change_theme.skin_plugin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
