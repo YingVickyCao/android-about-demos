2bb7368aef19bef03e47dc4f0ac399dc570325f0
Vicky <ying.vicky.cao@outlook.com>
2023-02-23 21:31:52 +0800

    。

--- test/src/main/java/com/hades/example/android/test/apk_upgrade/HttpConnectionNetManager.java
+++ test/src/main/java/com/hades/example/android/test/apk_upgrade/HttpConnectionNetManager.java
@@ -1,4 +1,20 @@
 package com.hades.example.android.test.apk_upgrade;
 
-public class HttpConnectionNetManager {
+import java.io.File;
+
+public class HttpConnectionNetManager implements INetManager {
+    @Override
+    public void get(String url, INetCallback callback, Object tag) {
+
+    }
+
+    @Override
+    public void download(String url, File targetFile, INetDownloadCallBack callBack, Object tag) {
+
+    }
+
+    @Override
+    public void cancel(Object tag) {
+
+    }
 }
