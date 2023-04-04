a3e8f34d5d1050396dda19ed47926e3d1ba5a1f7
Vicky <ying.vicky.cao@outlook.com>
2023-04-04 21:54:14 +0800

    refactor

---
+++ app/src/main/java/com/hades/example/android/_case/recognize_speech/google/TestRecognizerSpeechGoogleActivity.java
@@ -0,0 +1,6 @@
+package com.hades.example.android._case.recognize_speech.google;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestRecognizerSpeechGoogleActivity extends AppCompatActivity {
+}
