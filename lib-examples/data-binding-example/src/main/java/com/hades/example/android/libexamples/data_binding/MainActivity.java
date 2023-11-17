d50326bf3e3c8a6d2d46643ec717a4c7c76e9373
yingvickycao <ying.vicky.cao@outlook.com>
2023-11-17 14:31:38 +0800

    refactor

---
+++ lib-examples/data-binding-example/src/main/java/com/hades/example/android/libexamples/data_binding/MainActivity.java
@@ -0,0 +1,6 @@
+package com.hades.example.android.libexamples.data_binding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+}
--- test/src/main/java/com/hades/example/android/test/apk_upgrade/INetCallback.java
+++
@@ -1,7 +0,0 @@
-package com.hades.example.android.test.apk_upgrade;
-
-public interface INetCallback {
-    void success(String versionData);
-
-    void fail();
-}
