125e9bc304984ce9f634d515a42952cdc019681c
yingvickycao <ying.vicky.cao@outlook.com>
2023-11-25 13:49:03 +0800

    set change theme

--- custom_theme/src/main/java/com/hades/example/android/custom_theme/MainActivity.java
+++ custom_theme/src/main/java/com/hades/example/android/custom_theme/MainActivity.java
@@ -1,14 +1,43 @@
 package com.hades.example.android.custom_theme;
 
+import android.app.TaskStackBuilder;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    static boolean isLightTheme = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme();
         setContentView(R.layout.activity_main);
+        setThemeMode();
+
+        findViewById(R.id.changeTheme).setOnClickListener(view -> changeTheme());
+    }
+
+    private void changeTheme() {
+        isLightTheme = !isLightTheme;
+        finish();
+        TaskStackBuilder.create(this).addNextIntent(getIntent()).startActivities();
+    }
+
+    private void setTheme() {
+        if (isLightTheme) {
+            setTheme(R.style.LightAppTheme);
+        } else {
+            setTheme(R.style.DarkAppTheme);
+        }
+    }
+
+    private void setThemeMode() {
+        if (isLightTheme) {
+            ((TextView) findViewById(R.id.themeMode)).setText("Light Theme");
+        } else {
+            ((TextView) findViewById(R.id.themeMode)).setText("Dark Theme");
+        }
     }
 }
