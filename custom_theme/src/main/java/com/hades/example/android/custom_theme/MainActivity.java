71c2eada1cd8bbc1b3e5b19270a4d86ef208ada1
yingvickycao <ying.vicky.cao@outlook.com>
2023-11-25 13:36:37 +0800

    init

---
+++ custom_theme/src/main/java/com/hades/example/android/custom_theme/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hades.example.android.custom_theme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
