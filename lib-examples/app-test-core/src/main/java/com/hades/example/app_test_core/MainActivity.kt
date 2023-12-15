e7173f7efdb6c81b12a0b52683125ef8930a98ce
yingvickycao <ying.vicky.cao@outlook.com>
2023-12-15 12:05:02 +0800

    add test codes

---
+++ lib-examples/app-test-core/src/main/java/com/hades/example/app_test_core/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hades.example.app_test_core
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
