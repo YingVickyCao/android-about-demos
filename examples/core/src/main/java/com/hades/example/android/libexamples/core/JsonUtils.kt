1b74367f9eb9de8d572296f64ffa0de4c979645e
yingvickycao <ying.vicky.cao@outlook.com>
2023-12-17 16:37:53 +0800

    maven-publish

--- examples/core/src/main/java/com/hades/example/android/libexamples/core/JsonUtils.kt
+++ examples/core/src/main/java/com/hades/example/android/libexamples/core/JsonUtils.kt
@@ -2,14 +2,27 @@ package com.hades.example.android.libexamples.core
 
 import com.google.gson.GsonBuilder
 
-
+/**
+ * This is json util written by kotlin
+ */
 class JsonUtils {
     private var gson = GsonBuilder().enableComplexMapKeySerialization().create()
 
-    fun toJson(src: Any):String {
+    /**
+     * generate a json string
+     * @param src target class
+     * @return the json string
+     */
+    fun toJson(src: Any): String {
         return gson.toJson(src)
     }
 
+    /**
+     * Generate the object of given class from a json string
+     * @param value the json string
+     * @param classOfT class type
+     * @return the object of this class
+     */
     fun <T> fromJson(value: String, classOfT: Class<T>): T {
         return gson.fromJson(value, classOfT)
     }
