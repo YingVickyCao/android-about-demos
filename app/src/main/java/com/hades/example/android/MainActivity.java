61a88cbb7717f25d12ac76b632c8b0fa0148b9aa
YingVickyCao <yingvickycao@163.com>
2024-06-20 00:15:50 +0800

    refactor

--- app/src/main/java/com/hades/example/android/MainActivity.java
+++ app/src/main/java/com/hades/example/android/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void showCurrentTest() {
-        pageWidget();
+//        pageWidget();
     }
 
     private void pageSecurity() {
