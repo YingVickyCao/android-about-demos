512090d5d80562c3bdf26561b8a2441704193d0b
YingVickyCao <yingvickycao@163.com>
2024-07-08 20:44:47 +0800

    remove exoplayer from app

--- app/src/main/java/com/hades/example/android/test_libs/TestLibsActivity.java
+++ app/src/main/java/com/hades/example/android/test_libs/TestLibsActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import com.hades.example.android.R;
 import com.hades.example.android.base.BaseActivity;
-import com.hades.example.android.test_libs.exoplayer2.TestExoplayer2MainActivity;
 import com.hades.example.android.test_libs.okhttp.TestOkHttpActivity;
 
 public class TestLibsActivity extends BaseActivity {
@@ -14,12 +13,7 @@ public class TestLibsActivity extends BaseActivity {
         setContentView(R.layout.activity_libs);
         initViews();
 
-        findViewById(R.id.page_Exoplayer2).setOnClickListener(v -> page_Exoplayer2());
         findViewById(R.id.page_OkHttp).setOnClickListener(v -> page_OkHttp());
-    }
-
-    private void page_Exoplayer2() {
-        showActivity(TestExoplayer2MainActivity.class);
     }
 
     private void page_OkHttp(){
