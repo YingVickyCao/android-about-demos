3a819fcd3fe2fedd7dfa1e1cc60987a39eeb144b
YingVickyCao <yingvickycao@163.com>
2024-07-09 20:16:10 +0800

    define custom permission

--- examples/A/src/main/java/com/hades/example/android/a/TestCustomPermissionActivity.java
+++ examples/A/src/main/java/com/hades/example/android/a/TestCustomPermissionActivity.java
@@ -3,28 +3,66 @@ package com.hades.example.android.a;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.hades.utility.permission.OnContextUIListener;
+import com.hades.utility.permission.OnPermissionResultCallback;
+import com.hades.utility.permission.OnSimplePermissionCallback;
+import com.hades.utility.permission.PermissionsTool;
 
 // <permission>
-public class TestCustomPermissionActivity extends BaseActivity4Permission {
+public class TestCustomPermissionActivity extends AppCompatActivity {
     private static final String TAG = TestCustomPermissionActivity.class.getSimpleName();
+    PermissionsTool permissionsTool;
+    private String[] customPermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manifest_cusotom_permission_layout);
 
-        checkPermission("Request custom permission", "com.hades.example.android.b.CUSTOM_PERMISSION_TEST");
-        findViewById(R.id.jump).setOnClickListener(v -> jump());
+        findViewById(R.id.jump).setOnClickListener(v -> clickJump());
     }
 
-    @Override
-    protected boolean isNeedCheckPermission() {
-        return true;
+    // Define a custom app permission  https://developer.android.com/guide/topics/permissions/defining
+    private void clickJump() {
+        if (null == permissionsTool) {
+            permissionsTool = new PermissionsTool(this);
+        }
+        if (null == customPermission) {
+            customPermission = new String[]{"com.hades.example.android.b.CUSTOM_PERMISSION_TEST"};
+        }
+
+        permissionsTool.request(customPermission, new OnPermissionResultCallback() {
+            @Override
+            public void showInContextUI(OnContextUIListener callback) {
+                Snackbar.make(findViewById(R.id.root), "Request custom permission", Snackbar.LENGTH_SHORT).setAction("Allow", v -> callback.ok()).setAction("Cancel", v -> callback.cancel()).show();
+            }
+
+            @Override
+            public void onPermissionGranted() {
+                Snackbar.make(findViewById(R.id.root), "custom permission granted", Snackbar.LENGTH_SHORT).show();
+                jump();
+            }
+
+            @Override
+            public void onPermissionDenied() {
+                Snackbar.make(findViewById(R.id.root), "custom permission denied", Snackbar.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionError(String message) {
+                Snackbar.make(findViewById(R.id.root), "custom permission error", Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void jump() {
-        ComponentName componentName = new ComponentName("com.hades.example.android.b",
-                "com.hades.example.android.b.DActivity");
+        ComponentName componentName = new ComponentName("com.hades.example.android.b", "com.hades.example.android.b.DActivity");
         Intent intent = new Intent();
         intent.setComponent(componentName);
         intent.putExtra("NUM", 100);
